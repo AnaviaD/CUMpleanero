e87b8679295e586ed951fbd5648dd26c935e625c
Alberto Navia <anavia_n@outlook.com>
2022-07-24 20:14:48 -0500

    Add cumpleanero entity and ordenar lista de Cumpleaneros

---
+++ CUMpleaneroz/FTRDHLFR/CumpleaneroEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTRDHLFR
+{
+    internal class CumpleaneroEntity
+    {
+        public string IdEmployee { get; set; }
+        public string Nombre { get; set; }
+        public int fechaNacimiento { get; set; }
+    }
+}
--- CUMpleaneroz/FTRDHLFR/HoyCumplesAnos.cs
+++ CUMpleaneroz/FTRDHLFR/HoyCumplesAnos.cs
@@ -40,11 +40,12 @@ namespace FTRDHLFR
 
         public void ChecarSiCumplesAnosHoy()
         {
+            string FinalCollage = "";
             //this.UploadFiles();
             try
             {
                 DataTable dt = new DataTable();
-                string sentencia = string.Format("SELECT [idCliente],[nombre], [fechaNacimiento] FROM [Recursos].[dbo].[baseClientesHackaton2022] WHERE MONTH(fechaNacimiento) = DATEPART(MONTH,getdate())");
+                string sentencia = string.Format("SELECT TOP 20 [idCliente],[nombre], [fechaNacimiento] FROM [Recursos].[dbo].[baseClientesHackaton2022] WHERE MONTH(fechaNacimiento) = DATEPART(MONTH,getdate())");
                 this._xConnString.Open();
 
                 SqlDataAdapter dataAda = new SqlDataAdapter(sentencia, this._xConnString);
@@ -57,15 +58,86 @@ namespace FTRDHLFR
                     {
                         //StellantCopy.RemoveAt(i);
                     }
-                    
                 }
+
                 this.ConsoladeSalida = "La lista se pude leer, creo";
+                dt = OrdenarCumpleañosPorFecha(dt);
+                FinalCollage = HacerStringParaHtmlMail(dt);
+                this.ConsoladeSalida = FinalCollage;
             }
             catch
             {
                 this.ConsoladeSalida = "Problemas al obtener catalogos los archivos.";
             }
             
+        }
+
+        public DataTable OrdenarCumpleañosPorFecha(DataTable dtTable)
+        {
+            DataTable dataTable = new DataTable();
+
+
+            //Obtenemos la lista para ordenar
+            //Obtenemos la lista para ordenar
+            List<CumpleaneroEntity> OrdenList = new List<CumpleaneroEntity>();
+            foreach (DataRow e in dtTable.Rows)
+            {
+                //Obtenemos el cumpleaños del empleado en la string datehoy
+                string datehoy = e.ItemArray[2].ToString();
+                //Obtenemos el cumpleaños del empleado en la string datehoy
+                var parsedDate = DateTime.Parse(e.ItemArray[2].ToString());
+
+
+                CumpleaneroEntity ent = new CumpleaneroEntity();
+
+                //Asignando valores a la entidad
+                //Asignando valores a la entidad
+                ent.IdEmployee = e.ItemArray[0].ToString();
+                ent.Nombre = e.ItemArray[1].ToString();
+                ent.fechaNacimiento = parsedDate.Day;
+                //Asignando valores a la entidad
+                //Asignando valores a la entidad
+
+                OrdenList.Add(ent);
+            }
+            //Obtenemos la lista para ordenar
+            //Obtenemos la lista para ordenar
+
+
+            //Codigo para ordenar la lista
+            //Codigo para ordenar la lista
+            //Codigo para ordenar la lista
+            //Codigo para ordenar la lista
+
+
+            return dtTable;
+        }
+
+        public string HacerStringParaHtmlMail(DataTable dtTable)
+        {
+            string CollageMensajes = "";
+            // 0 = Idempl
+            // 1 = nombre
+            // 2 = fecha
+            foreach (DataRow e in dtTable.Rows)
+            {
+                //String.Format("El archivo no pudo leerse Correctamente{0}", "<br />");
+                //Acomoda El nombre 
+                if ((e.ItemArray[1].ToString() != null) && (e.ItemArray[1].ToString() != ""))
+                {
+                    CollageMensajes += String.Format("{0}   --", e.ItemArray[1].ToString()); 
+                }
+                //Acomoda La fecha
+                if ((e.ItemArray[2].ToString() != null) && (e.ItemArray[2].ToString() != ""))
+                {
+                    //DateTime DT1 = new DateTime(datehoy);
+                    //var cultureInfo = new CultureInfo("es-ES");
+                    string datehoy = e.ItemArray[2].ToString();
+                    var parsedDate = DateTime.Parse(datehoy);
+                    CollageMensajes += String.Format("{0} - {1} {2}",parsedDate.Day, parsedDate.Month, "<br />");
+                }
+            }
+            return CollageMensajes;
         }
     }
 }
